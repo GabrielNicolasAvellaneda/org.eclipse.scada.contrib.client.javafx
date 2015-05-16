e7a39256b1ab8e88fd31c6aec632452191c0ba07
Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>
2015-05-16 12:40:04 -0300

    Initial project version
    Signed-off-by: Gabriel Nicolas Avellaneda <avellaneda.gabriel@gmail.com>

---
+++ src/org/eclipse/scada/contrib/client/javafx/views/WindowViewPart.java
@@ -0,0 +1,16 @@
+package org.eclipse.scada.contrib.client.javafx.views;
+
+import org.eclipse.fx.ui.workbench3.FXViewPart;
+import javafx.scene.Scene;
+
+public class WindowViewPart extends FXViewPart {
+	@Override
+	protected Scene createFxScene() {
+		return null;
+	}
+
+	@Override
+	protected void setFxFocus() {
+		
+	}
+}
